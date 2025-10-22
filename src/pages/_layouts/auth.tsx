80258506192a3fc5580ada4fb2ec18f6ddb7e742
aaron24-web <Aarontunwork24@gmail.com>
2025-10-21 21:41:36 -0600

    fondo login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,10 @@ import { Link, Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2 antialiased">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+      <div
+        className="flex h-full flex-col justify-between border-r border-foreground/5 p-10 text-muted-foreground bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(/Fondo_Napoli_Login.jpg)` }}
+      >
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <img src="/Barrio_Napoli_Logo.png" alt="Barrio Napoli Logo" className="h-8 w-8" />
           <span className="font-semibold">Barrio Napoli</span>
